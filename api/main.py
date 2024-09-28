bae93e8840346a29802ee49ec9621f6ec9f1c696
Priyansh Patel <youname972@gmail.com>
2024-09-28 19:56:51 -0400

    Tranformed data

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from httplib2 import Credentials
+from google.oauth2.credentials import Credentials
 import uvicorn
 from typing import Union
 from dotenv import load_dotenv
-from services.gcal.upload import createEvent, createOrClearCalender
+from services.gcal.upload import create_event_times, createEvent, createOrClearCalender
 from services.llm.gemini import time_per_course
 from services.llm.models import QueryBody
 from googleapiclient.discovery import build
@@ -19,18 +19,30 @@ def read_root():
 
 @app.post("/api/generate")
 def generate(query: QueryBody):
-   LLM_response = time_per_course(query["data"])
+   LLM_response = time_per_course(query.data)
    scheduled_data = LLM_response.dict()
 
    # Create Credentials object using the access token
-   creds = Credentials(token=query["token"])
+   creds = Credentials(token=query.access_token)
 
    # Build the Google Calendar service
    service = build('calendar', 'v3', credentials=creds)
    calendarId = createOrClearCalender(service)
 
-   for assignment in scheduled_data:
-      createEvent(service, calendarId, assignment)
+   for course in scheduled_data:
+      for assignment in course["data"]:
+         event_times = create_event_times(assignment)
+
+         createEvent(service, calendarId, {
+            "summary": assignment["assignmentName"],
+            "description": course["courseName"],
+            "start": {
+               "datetime": event_times["start"]["datetime"]
+            },
+            "end": {
+               "datetime": event_times["end"]["datetime"]
+            }
+         })
 
    return {
       "status_code": 200
