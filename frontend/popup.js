c7a4c9a8420fb2181fcd21d6449747f19880aa6f
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2024-09-29 10:55:07 -0400

    fix token

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,5 +1,5 @@
 var data = {
-    'access_token': "ya29.a0AcM612yAPfnrE-MzWstuyVXgwkzufQOVv35qr0Lds2apliQajNPbmRzXucZYvnJEH7Se60677OnnOhdVxWvq4ApsIB_hJAGRop569S3eYQB0xP2i0eq-l8l4CcCdrzvTzJgFftKmDyAmXwsk3nXdie2rbRLsavHQfhZa9ij0aCgYKASgSARMSFQHGX2MiBqZmGNyzGWp37g8dP6DxzA0175",
+    'access_token': "ya29.a0AcM612z-s88qdgz6340g6_GBKU58hsc3FQBw8B7iUodOypirgNadbP86GNrOMIpbqZLS4dQF9J7ttO1Y4VfCqtPvoPb-Zek7fTN02TC70bY_6eZUuwmh4TVGwKZT9hgYdXx1xSX4DZbBH1llwBYWbxwfHDGryJMZdQDtqQLpaCgYKAWMSARMSFQHGX2Mii2dNClF4aiO06trqxNc7BQ0175",
     "data": [
     {
         "courseName" : "Principles of Operating Systems",
